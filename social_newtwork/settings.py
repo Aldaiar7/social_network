987773a0971987696a55bed865dee8790e3309d1
Aldaiar <aldaiardushebaev@gmail.com>
2022-09-28 16:00:12 +0600

    added custom middleware to disable browser caching

--- social_newtwork/settings.py
+++ social_newtwork/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "social_newtwork.middlewares.DisableClientSideCachaMiddleware",
 ]
 
 ROOT_URLCONF = "social_newtwork.urls"
